4a0a960c6e520256cf915e8d20598a6a504b5b2c
vishveshsoni <vishveshsoni54@gmail.com>
2020-04-20 09:22:15 +0530

    reverse changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,22 @@ let protocol = location.protocol + '//' + location.host;
 
 export var APP_CONFIG = {};
 
-export var ,Z
+export var URL_CONFIG = {
+  BASE_URL: getConfigs()['BASE_URL'],
+  LOGIN: '/login',
+  SIGNUP: '/signup',
+  GET_QUESTIONS_BY_DAY: '/get-questions-by-day',
+  CREATE_NEW_TRACKER: '/create-new-tracker',
+  GET_HEALTH_TRACKER: '/get-health-tracker',
+  SAVE_DATA_FOR_DAY: '/save-data-for-day',
+  GET_USER_INFO: '/get-user-info',
+  LOGOUT:'/logout'
 };
 
 export function getConfigs() {
   if (protocol == 'http://localhost:4200') {
     return {
-      BASE_URL:  'http://localhost:5000'
+      BASE_URL:  'http://3.86.171.251:5000'
     };
   } else {
     return {
