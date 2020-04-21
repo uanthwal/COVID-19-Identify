68a522ffefe518b3f8b2b21134d2447e99fd7f9d
uanthwal <uanthwal@gmail.com>
2020-04-20 21:53:34 -0300

    Build after map zoom changes

--- dist/identify/main-es5.js
+++ dist/identify/main-es5.js
@@ -403,7 +403,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     function getConfigs() {
       if (protocol == 'http://localhost:4200') {
         return {
-          BASE_URL: 'http://localhost:5000'
+          BASE_URL: 'http://3.86.171.251:5000'
         };
       } else {
         return {
@@ -1591,10 +1591,10 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
         this.options = {
           layers: [L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            maxZoom: 18,
+            maxZoom: 17,
             attribution: ""
           })],
-          zoom: 17,
+          zoom: 15,
           center: L.latLng(44.641467, -63.587546)
         };
       }
@@ -1686,7 +1686,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     //Should be this data set: http://data.linz.govt.nz/#/layer/779-nz-street-address-electoral/
 
 
-    var addressPoints = [[44.645849, -63.595128, 1.0], [44.635850, -63.575129, 2.0], [44.6558551, -63.495130, 3.0], [44.642604, -63.587413, 4.0], [44.641467, -63.587546, 5.0], [44.641567, -63.587646, 4.0], [44.641367, -63.587346, 5.0], [44.658180, -63.593669, 2.0], [44.655908, -63.589010, 1.0], [44.654487, -63.614898, 3.0], [44.667978, -63.569711, 4.0]];
+    var addressPoints = [[44.645849, -63.595128, 1.0], [44.635850, -63.575129, 2.0], [44.6558551, -63.495130, 3.0], [44.642604, -63.587413, 4.0], [44.641467, -63.587546, 5.0], [44.641567, -63.587646, 4.0], [44.641267, -63.587346, 4.0], [44.641167, -63.587246, 4.0], [44.641667, -63.587746, 4.0], [44.641767, -63.587846, 4.0], [44.641867, -63.587946, 4.0], [44.641367, -63.587246, 5.0], [44.658180, -63.593669, 2.0], [44.655908, -63.589010, 1.0], [44.654487, -63.614898, 3.0], [44.667978, -63.569711, 4.0]];
     /***/
   },
 
